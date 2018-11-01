1e693df1b2f55675557e2755a2a62e59038f2660
nicmosc <nmoscholios@gmail.com>
2018-11-01 17:31:30 +0100

    finish styling theme switcher

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -11,6 +11,9 @@ const styles = {
     border-right: 1px solid var(--line-color);
     transition: all var(--transition-duration) ease-in-out;
     transition-property: background, border-right;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
   `,
   closed: css`
     width: 100px;
@@ -63,10 +66,39 @@ const styles = {
     height: 20px;
     margin-right: calc(var(--margin) / 2);
   `,
-  label: css`
-
-  `,
   themeSwitcher: css`
+    margin: calc(var(--margin) * 2);
+    margin-top: auto;
+    display: flex;
+    align-items: center;
+    color: var(--text-primary);
+    transition: all var(--transition-duration-short) ease-in-out;
+
+    &:hover {
+      cursor: pointer;
+      color: var(--primary);
+    }
+  `,
+  themeIcon: css`
+    > * {
+      display: none;
+    }
+  `,
+  darkActive: css`
+    [data-element="sun"] {
+      display: none;
+    }
+    [data-element="moon"] {
+      display: block;
+    }
+  `,
+  lightActive: css`
+    [data-element="sun"] {
+      display: block;
+    }
+    [data-element="moon"] {
+      display: none;
+    }
   `,
 };
 
@@ -81,9 +113,7 @@ const NavigationButton: React.SFC<{
     <Link to={to} style={{ textDecoration: 'none' }}>
       <div className={cx(styles.navigationButton, { [styles.active]: active })}>
         <i className={styles.icon} data-feather={icon} />
-        <div className={styles.label}>
-          {label}
-        </div>
+        <span>{label}</span>
       </div>
     </Link>
   );
@@ -109,7 +139,14 @@ const SideBar: React.SFC<{
         <NavigationButton label="People" icon="users" to="/people" active={pathname === '/people'} />
       </div>
       <div className={styles.themeSwitcher} onClick={toggleTheme}>
-        {activeTheme}
+        <div className={cx(styles.themeIcon, {
+          [styles.darkActive]: activeTheme === 'dark',
+          [styles.lightActive]: activeTheme === 'light',
+        })}>
+          <i className={styles.icon} data-feather="sun" data-element="sun" />
+          <i className={styles.icon} data-feather="moon" data-element="moon" />
+        </div>
+        <span>{activeTheme === 'light' ? 'Light mode' : 'Dark mode'}</span>
       </div>
     </div>
   );
