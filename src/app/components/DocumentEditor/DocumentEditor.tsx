fae9fa52cad95a1dc5dc3e37955c49d54984d79f
nicmosc <nmoscholios@gmail.com>
2018-12-04 21:41:50 +0100

    can select a particular page of the document

--- src/app/components/DocumentEditor/DocumentEditor.tsx
+++ src/app/components/DocumentEditor/DocumentEditor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 import { BlobProvider } from '@react-pdf/renderer';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import autobind from 'autobind-decorator';
@@ -30,21 +30,54 @@ const styles = {
     z-index: 9;
   `,
   page: css`
+    position: relative;
     margin: calc(var(--margin) * 2) 0;
     box-shadow: var(--box-shadow);
+    transition: all var(--transition-duration-short) ease-in-out;
+
+    &::after {
+      content: ' ';
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background: var(--primary);
+      pointer-events: none;
+      opacity: 0;
+      transition: all var(--transition-duration-short) ease-in-out;
+    }
+  `,
+  selected: css`
+    box-shadow: var(--box-shadow), 0px 0px 0px 4px var(--primary);
+
+    &::after {
+      opacity: 0.2;
+    }
   `,
 }
 
 
 class DocumentEditor extends React.Component {
+  pages = {}
+
   state = {
     zoom: 1.0,
     pages: 0,
     activePage: 1,
+    editingPage: undefined,
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this._handleClickPage);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this._handleClickPage);
   }
 
   render() {
-    const { zoom, pages } = this.state;
+    const { zoom, pages, editingPage } = this.state;
     return (
       <div className={styles.documentEditor}>
         <div className={styles.navigationBar}>
@@ -62,7 +95,12 @@ class DocumentEditor extends React.Component {
                   {blob ?
                     <Document file={blob} onLoadSuccess={this._onDocumentLoadSuccess}>
                       {Array(pages).fill(0).map((value, index) => (
-                        <Page className={styles.page} key={index} pageNumber={index + 1} scale={zoom} />
+                        <Page
+                          inputRef={(page: HTMLElement) => this.pages[`page${index}`] = page}
+                          className={cx(styles.page, { [styles.selected]: editingPage === index })}
+                          key={index}
+                          pageNumber={index + 1}
+                          scale={zoom} />
                       ))}
                     </Document>
                   : 'Loading...'}
@@ -73,6 +111,28 @@ class DocumentEditor extends React.Component {
         </div>
       </div>
     );
+  }
+
+  @autobind
+  _handleClickPage(e: MouseEvent) {
+    const boundingBoxes = Object.values(this.pages).map((page: HTMLElement) => page.getBoundingClientRect());
+    if (boundingBoxes.length > 0) {
+      const xDelimiter = { left: boundingBoxes[0].left, right: boundingBoxes[0].left + boundingBoxes[0].width };
+      if (e.clientX > xDelimiter.left && e.clientX < xDelimiter.right) {
+        const yDelimiters = boundingBoxes.map((box) => ({ top: box.top, bottom: box.top + box.height }));
+        let page = 0;
+        for (let delimiter of yDelimiters) {
+          if (e.clientY > delimiter.top && e.clientY < delimiter.bottom) {
+            this.setState({ editingPage: page });
+            break;
+          }
+          page++;
+        }
+      }
+      else {
+        this.setState({ editingPage: undefined });
+      }
+    }
   }
 
   @autobind
