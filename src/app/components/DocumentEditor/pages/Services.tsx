0855d7a9db08bbef4eec8f9ca0b872fd0c5e2678
nicmosc <nmoscholios@gmail.com>
2018-12-23 15:17:19 +0100

    add alternaating service sections

--- src/app/components/DocumentEditor/pages/Services.tsx
+++ src/app/components/DocumentEditor/pages/Services.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Image, Text } from '@react-pdf/renderer';
 
-// import sv from '../vars';
+import sv from '../vars';
 import PageWrapper from './PageWrapper';
 import { getCurrentLocale } from  '~/utils';
 import { createTranslate, translate as t, translateAlt as ta } from '~/utils/translation';
@@ -42,23 +42,73 @@ function generateServiceSections(allServices: string[], products: any, locale: s
 
 
 const styles = StyleSheet.create({
-  wrapper: {
-  },
   service: {
-
+    width: '100%',
+    height: 220,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: sv.baseMargin * 2,
+  },
+  reversed: {
+    flexDirection: 'row-reverse',
+  },
+  info: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: sv.baseMarginSmall,
+  },
+  reversedInfo: {
+    marginLeft: sv.baseMarginSmall,
+    marginRight: 0,
+  },
+  imageWrapper: {
+    marginRight: -120,
+    width: 330,
+    marginLeft: sv.baseMargin,
+  },
+  reversedImage: {
+    marginRight: sv.baseMargin,
+    marginLeft: -120
+  },
+  image: {
+    height: '100%',
+  },
+  icon: {
+    height: 30,
+  },
+  title: {
+    color: sv.textPrimary,
+    marginBottom: sv.baseMarginSmall,
+    marginTop: 3,
+  },
+  description: {
+    fontSize: 10,
+    color: sv.textPrimary,
+    textAlign: 'center',
+    lineHeight: 1.8,
   },
 });
 
 
 const Service: React.SFC<{
   service: ServiceType,
-}> = ({ service }) => {
+  reversed?: boolean,
+}> = ({ service, reversed=false }) => {
+  const { icon, name, coverImage, description } = service;
   return (
-    <View style={styles.service}>
-      <View style={styles.image}>
-        <Image />
+    <View style={[styles.service, reversed ? styles.reversed : null]} wrap={false}>
+      <View style={[styles.info, reversed ? styles.reversedInfo : null]}>
+        <Image src={icon} style={styles.icon} />
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.description}>{description}</Text>
       </View>
-      <Text></Text>
+      <View style={[styles.imageWrapper, reversed ? styles.reversedImage : null]}>
+        <Image src={coverImage} style={styles.image} />
+      </View>
     </View>
   );
 };
@@ -77,13 +127,13 @@ const Services: React.SFC<{
     name: t(locale, 'services.interior3d.name'),
     description: t(locale, 'services.interior3d.description'),
     coverImage: require('../images/services/interior3d.jpg'),
-    icon: '',
+    icon: require('../images/icons/services/interior3d.png'),
   };
   return (
     <PageWrapper title="Drawbotics" subtitle={tt(locale, 'title')} wrap>
-      <View style={styles.wrapper}>
-        <Service service={service} />
-      </View>
+      <Service service={service} />
+      <Service service={service} reversed />
+      <Service service={service} />
     </PageWrapper>
   );
 };
