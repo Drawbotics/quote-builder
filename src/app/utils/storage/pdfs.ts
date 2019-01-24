93bbb252541eed70eb27dff99f521737f6202653
nicmosc <nmoscholios@gmail.com>
2019-01-24 17:17:56 +0100

    save mappings for pdfs too

--- src/app/utils/storage/pdfs.ts
+++ src/app/utils/storage/pdfs.ts
@@ -1,5 +1,5 @@
-import { save } from './index';
-import { writeFile } from '../index';
+import { save, load, remove } from './index';
+import { readFile, writeFile, deleteFile } from '../index';
 
 
 async function saveMapping(id: string, path: string) {
@@ -7,17 +7,50 @@ async function saveMapping(id: string, path: string) {
 }
 
 
-// async function loadMappings() {
-//   return await load('pdf-mappings');
-// }
-//
-//
-// async function removeMapping(id: string) {
-//   return await remove('pdf-mappings', id);
-// }
+async function loadMappings() {
+  return await load('pdf-mappings');
+}
 
 
-export async function savePDF(filename: string, path: string, value: any) {
+async function removeMapping(id: string) {
+  return await remove('pdf-mappings', id);
+}
+
+
+export async function loadPDFs() {
+  const mappings = await loadMappings();
+  if (! mappings) return {};
+  const fileIds = Object.keys(mappings);
+  let files = {};
+  let notFound: string[] = [];
+  for (const id of fileIds) {
+    const file = await readFile(mappings[id]).catch(() => {
+      notFound.push(id);
+    });
+    if (file) {
+      files[id] = JSON.parse(file);
+    }
+  }
+  return { files, notFound };
+}
+
+
+export async function getPDFLocation(id: string) {
+  const mappings = await loadMappings();
+  const location = mappings[id];
+  return location;
+}
+
+
+export async function savePDF(id: string, path: string, value: any) {
   await writeFile(path, value);
-  await saveMapping(filename, path);
+  await saveMapping(id, path);
+}
+
+
+export async function deletePDF(id: string) {
+  // TODO handle case where file is deleted outside of flow and we try to delete here (file not found)
+  const location = await getPDFLocation(id);
+  await deleteFile(location);
+  await removeMapping(id);
 }
