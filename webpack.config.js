ad52b541230a576e60346a290b0a400b18f3ac12
nicmosc <nmoscholios@gmail.com>
2019-01-26 20:33:59 +0100

    fix build assets

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
+    publicPath: '/',
     globalObject: 'this',
   },
   resolve: {
