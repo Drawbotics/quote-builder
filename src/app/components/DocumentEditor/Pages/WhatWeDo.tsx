ace5a1ff583d5a5f65582986fc7ae12dccec4cb4
nicmosc <nmoscholios@gmail.com>
2019-02-14 14:36:00 +0100

    small fixes

--- src/app/components/DocumentEditor/Pages/WhatWeDo.tsx
+++ src/app/components/DocumentEditor/Pages/WhatWeDo.tsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: sv.baseMargin * 3,
+    marginTop: sv.baseMargin * 2,
   },
   section: {
     position: 'relative',
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: sv.baseMargin * 2,
+    marginBottom: sv.baseMargin,
   },
   reverse: {
     flexDirection: 'row-reverse',
